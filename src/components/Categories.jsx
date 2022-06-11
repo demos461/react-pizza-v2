df7b69e9b6fbda2acd3bb0d4557b06f05ac25dde
Ilya Khrapko <iamdemos461@gmail.com>
2022-06-11 18:19:29 +0300

    create change active category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
+
 export const Categories = () => {
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытие'];
+  const [activeCategory, setActiveCategory] = useState(0);
+
+  const onClickCategory = index => {
+    setActiveCategory(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category, index) => (
+          <li
+            key={`${category}-${index}`}
+            className={activeCategory === index ? 'active' : null}
+            onClick={() => onClickCategory(index)}
+          >
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
