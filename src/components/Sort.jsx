6eab2578facf3f7a4179b9de8dc844409303b198
demos461 <iamdemos461@gmail.com>
2023-02-21 19:12:37 +0300

    add handleClickOutside to hide popup sort

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 export const Sort = () => {
@@ -11,6 +11,7 @@ export const Sort = () => {
   const [activeSort, setActiveSort] = useState(sortList[0].name);
   const [isOpen, setIsOpen] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
+  const sortRef = useRef();
 
   const categoryIndex = searchParams.get('category') || '';
   const sortParam = searchParams.get('sortBy') || '';
@@ -21,26 +22,37 @@ export const Sort = () => {
     setIsOpen(false);
   };
 
+  useEffect(() => {
+    const handleClickOutside = e => {
+      if (!e.composedPath().includes(sortRef.current)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.body.addEventListener('click', handleClickOutside);
+    return () => document.body.removeEventListener('click', handleClickOutside);
+  }, []);
+
   return (
-    <div className="sort">
-      <div className="sort__label">
+    <div ref={sortRef} className='sort'>
+      <div className='sort__label'>
         <svg
-          width="10"
-          height="6"
-          viewBox="0 0 10 6"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
+          width='10'
+          height='6'
+          viewBox='0 0 10 6'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
         >
           <path
-            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-            fill="#2C2C2C"
+            d='M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z'
+            fill='#2C2C2C'
           />
         </svg>
         <b>Сортировка по:</b>
         <span onClick={() => setIsOpen(!isOpen)}>{activeSort}</span>
       </div>
       {isOpen && (
-        <div className="sort__popup">
+        <div className='sort__popup'>
           <ul>
             {sortList.map(obj => (
               <li
