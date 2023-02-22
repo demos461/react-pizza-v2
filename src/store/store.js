b6d2e61757d443b03813bfcd11576d914de53db6
demos461 <iamdemos461@gmail.com>
2023-02-22 17:12:45 +0300

    create redux logic for the cart, create cartItem and cartEmpty components

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import filter from './slices/filterSlice';
 import pizzas from './slices/pizzasSlice';
+import cart from './slices/cartSlice';
 
 export const store = configureStore({
   reducer: {
     filter,
     pizzas,
+    cart,
   },
 });
